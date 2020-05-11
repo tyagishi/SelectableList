96ff336c1be76d434d4c6982bc66e6e2e3bcdca1
Tomoaki Yagishita <tyagishi@gmail.com>
2020-05-11 17:12:46 +0900

    initial commit

--- Sources/SelectableList/SelectableList.swift
+++ Sources/SelectableList/SelectableList.swift
@@ -1,3 +1,58 @@
-struct SelectableList {
-    var text = "Hello, World!"
+//
+//  SelectableList.swift
+//  ClockClipSwiftUI
+//
+//  Created by Tomoaki Yagishita on 2020/03/20.
+//  Copyright © 2020 SmallDeskSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+public struct SelectableList<T:Comparable & Hashable>: View {
+    var content:[T]
+    @Binding var selectedItem:T?
+    var rowCell:(T)-> Text
+    var selectionAction:(_ item: T)->Void
+    public var body: some View {
+        List {
+            ForEach(content, id: \.self) { item in
+                SelectableCell(item: item, selectedItem: self.$selectedItem,
+                               rowCell: self.rowCell, selectionAction: self.selectionAction)
+            }
+        }
+    }
+}
+
+public struct SelectableCell<T:Comparable> : View {
+    var item: T
+    @Binding var selectedItem: T?
+    var rowCell:(T)->Text
+    var selectionAction:(_ selected: T)->Void
+
+    public var body: some View {
+        HStack {
+            if ( item == selectedItem ) {
+                Image(systemName: "checkmark").foregroundColor(.accentColor)
+            }
+            Spacer()
+            self.rowCell(self.item)
+            Spacer()
+        } .onTapGesture {
+            self.selectedItem = self.item
+            self.selectionAction(self.item)
+        }
+    }
+}
+
+struct SelectableList_Previews: PreviewProvider {
+    @State static var contentExample = ["test1", "test2"]
+    @State static var selectedString:String? = "test1"
+    static var previews: some View {
+        SelectableList(content: contentExample, selectedItem: $selectedString, rowCell: { item -> Text in
+            Text(item)
+                }
+            , selectionAction: { item in
+                print("selected \(item)")
+        })
+    }
 }
